fc65f7013c95a3ad7c30de454332e163b05fdc7c
nutllwhy <linli0301@hotmail.com>
2018-07-12 10:06:48 +0800

    day 52

---
+++ Medium/1Array and string/3Crossword grouping.py
@@ -0,0 +1,20 @@
+# 字谜分组
+# 给定一个字符串数组，将字母异位词组合在一起。
+# 字母异位词指字母相同，但排列不同的字符串。
+
+class Solution:
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        # 字典，key为每一个单词排序后的字符串
+		bucket = {}
+		for ele in strs:
+			key = "".join(sorted(ele))
+			if key not in bucket.keys():
+				bucket[key] = [ele]
+			else:
+				bucket[key].append(ele)
+		strs_new = [value for value in bucket.values()]
+		return strs_new
