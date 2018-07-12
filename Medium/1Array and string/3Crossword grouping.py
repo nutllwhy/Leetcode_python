77e5d0b922a573ac7ee543ef5410267e8090b1de
nutllwhy <linli0301@hotmail.com>
2018-07-12 10:29:20 +0800

    day 54

--- Medium/1Array and string/3Crossword grouping.py
+++ Medium/1Array and string/3Crossword grouping.py
@@ -10,12 +10,12 @@ class Solution:
         """
         # 字典，key为每一个单词排序后的字符串
         # https://juejin.im/pin/5ad07aa5092dcb5883283ffc
-		bucket = {}
-		for ele in strs:
-			key = "".join(sorted(ele))
-			if key not in bucket.keys():
-				bucket[key] = [ele]
-			else:
-				bucket[key].append(ele)
-		strs_new = [value for value in bucket.values()]
-		return strs_new
+        bucket = {}
+        for ele in strs:
+            key = "".join(sorted(ele))
+            if key not in bucket.keys():
+                bucket[key] = [ele]
+            else:
+                bucket[key].append(ele)
+        strs_new = [value for value in bucket.values()]
+        return strs_new
